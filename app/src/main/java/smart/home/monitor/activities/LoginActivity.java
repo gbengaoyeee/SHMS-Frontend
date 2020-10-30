947bc93d29325860d669c0a82df579a223bd4fd6
n01326482 <n01326482@humbermail.ca>
2020-10-30 00:10:05 -0400

    Done login page layout. trying to make it responsive

--- app/src/main/java/smart/home/monitor/activities/LoginActivity.java
+++ app/src/main/java/smart/home/monitor/activities/LoginActivity.java
@@ -9,24 +9,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        button2 = (Button) findViewById(R.id.loginButton);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openHomePage();
-            }
-        });
-    }
-
-    public void openHomePage(){
-        Intent intent = new Intent(this, HomePage.class);
-        startActivity(intent);
     }
 
 }
