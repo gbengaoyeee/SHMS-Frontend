30b7a8d5e30ee7707b70805999be3853d181c926
n01326482 <n01326482@humbermail.ca>
2020-10-30 17:26:35 -0400

    done signup page layout fully

--- app/src/main/java/smart/home/monitor/activities/LoginActivity.java
+++ app/src/main/java/smart/home/monitor/activities/LoginActivity.java
@@ -7,13 +7,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView createAcct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        createAcct = (TextView) findViewById(R.id.createAcct);
+        createAcct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                segueToRegisterActivity();
+            }
+        });
+    }
 
+    /**
+     * Segues into Register activity
+     */
+    private void segueToRegisterActivity(){
+        Intent registerActivity = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerActivity);
     }
 
 }
