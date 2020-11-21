e5cabdd388515288c18f8f92d1e2025aaad7ffe1
n01326482 <n01326482@humbermail.ca>
2020-11-21 00:34:50 -0500

    pretty much done front end

---
+++ app/src/main/java/smart/home/monitor/activities/ReadingsActivity.java
@@ -0,0 +1,18 @@
+package smart.home.monitor.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import smart.home.monitor.R;
+
+public class ReadingsActivity extends AppCompatActivity {
+    TextView temperatureReading, gasReading, humidityReading;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_readings);
+    }
+}
